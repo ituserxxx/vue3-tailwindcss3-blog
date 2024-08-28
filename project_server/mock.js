ec16da5bad8fc69d73f22a45df9414fe252cd9ae
root <root@xiaoxx.utek.com>
2024-08-28 16:13:39 +0800

    debug blog front api and front

--- project_server/mock.js
+++ project_server/mock.js
@@ -3,7 +3,7 @@ var http = require('http')
 
 // import http from 'http';
 var querystring = require('querystring')
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
   response.setHeader('Access-Control-Allow-Origin', '*')
   // 允许的header类型
   response.setHeader('Access-Control-Allow-Headers', 'content-type')
@@ -13,11 +13,11 @@ http.createServer(function(request, response) {
   let data = {}
 
   // 2.注册data事件接收数据（每当收到一段表单提交的数据，该方法会执行一次）
-  request.on('data', function(chunk) {
+  request.on('data', function (chunk) {
     data += chunk
   })
 
-  request.on('end', function() {
+  request.on('end', function () {
     data = decodeURI(data)
     console.log(data)
     var dataObject = querystring.parse(data)
@@ -27,49 +27,134 @@ http.createServer(function(request, response) {
   console.log('请求url->' + request.url)
   if (url === '/blog/article/list') {
     data = articleList()
+    console.log(data)
   }
-  
+  if (url === '/blog/article/detail') {
+    data = articleDetail()
+  }
+  if (url === '/blog/tag/list') {
+    data = tagList()
+  }
+  if (url === '/blog/leaveMessage/list') {
+    data = leaveMessageList()
+  }
+  if (url === '/blog/leaveMessage/add') {
+    data = leaveMessageAdd()
+  }
   // 内容类型: text/plain
   response.writeHead(200,
     { 'Content-Type': 'application/json; charset=UTF-8' })
   // 发送响应数据
   response.end(JSON.stringify({
-    code: 20000,
+    code: 0,
     msg: true,
     data: data
   }))
 }).listen(6008)
 
-
-function articleList() {
-  return{
-    articleList: [
-        {
-            id: 1,
-            date: 'Jun 1, 2024',
-            title: 'Build Your New Idea with Vue 3 Framework.',
-            body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!',
-        },
-        {
-            id: 2,
-            date: 'Jul 15, 2024',
-            title: 'Explore the Latest Features of Vue 3',
-            body: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        },
-        {
-            id: 2,
-            date: 'Jul 15, 2024',
-            title: 'Explore the Latest Features of Vue 3',
-            body: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        },
-        {
-            id: 2,
-            date: 'Jul 15, 2024',
-            title: 'Explore the Latest Features of Vue 3',
-            body: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        }
-    ],
-    total:10
+function leaveMessageAdd() {
+  return {
+    id: 998,
+    name: "Michael Gough1111",
+    content: `   Very straight-to-point article. Really worth time reading. Thank
+            you! But tools are just the
+            instruments for the UX designers. The knowledge of the design tools are as important as the
+            creation of the design strategy.`,
+    date: "2022-03-04 15:12:33",
+  }
 }
+function leaveMessageList() {
+  return {
+    list: [
+      {
+        id: 1,
+        name: "Michael Gough",
+        content: `   Very straight-to-point article. Really worth time reading. Thank
+            you! But tools are just the
+            instruments for the UX designers. The knowledge of the design tools are as important as the
+            creation of the design strategy.`,
+        date: "2022-03-04 15:12:33",
+      },
+      {
+        id: 2,
+        name: "Michael Gough2",
+        content: `   Very straight-to-point article. Really worth time reading. Thank you!  `,
+        date: "2024-03-04 15:11:33",
+      }
+    ]
+  }
+}
+function tagList() {
+  return {
+    list: [
+      { name: 'Country Names', id: 1 },
+      { name: 'Chemistry', id: 2 },
+      { name: 'File Type', id: 3 },
+      { name: 'Cryptocurrency', id: 4 },
+      { name: 'Academic', id: 5 },
+      { name: 'Softwares', id: 6 },
+      { name: 'General', id: 7 },
+      { name: 'Web Technology', id: 8 },
+      { name: 'Business', id: 9 },
+      { name: 'Technology', id: 10 },
+      { name: 'Sports', id: 11 },
+      { name: 'Law', id: 12 },
+      { name: 'Internet Slangs', id: 13 },
+      { name: 'Insurance', id: 14 },
+      { name: 'Space Science', id: 15 },
+      { name: 'Jobs', id: 16 },
+      { name: 'Certifications', id: 17 },
+      { name: 'Electronics', id: 18 },
+    ]
+  }
+}
+function articleDetail() {
+  return {
+    id: 1,
+    date: "2022-05-06 23:59:59",
+    view: 312,
+    title: "Portrait Photography In Early Days",
+    content: "编辑文件：vim /etc/docker/daemon.json \n\n输入以下内容\n```go\n\n{\n  \"registry-mirrors\": [\n    \"https://ustc-edu-cn.mirror.aliyuncs.com\",\n    \"https://mirror.baidubce.com\",\n     \"https://docker.996.ninja\",\n    \"https://quay.996.ninja\",\n    \"https://k8s.996.ninja\",\n    \"https://k8s-gcr.996.ninja\",\n    \"https://ghcr.996.ninja\",\n    \"https://cloudsmith.996.ninja\"\n  ]\n}\n\n```\n然后重启docker\n```\nsystemctl restart docker\n```",
+    tags: [
+      {
+        id: 1,
+        name: "Joe"
+      }, {
+        id: 2,
+        name: "Election2020"
+      },
+    ]
+  }
+}
+function articleList() {
+  return {
+    articleList: [
+      {
+        id: 1,
+        date: 'Jun 1, 2024',
+        title: 'Build Your New Idea with Vue 3 Framework.',
+        body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!',
+      },
+      {
+        id: 2,
+        date: 'Jul 15, 2024',
+        title: 'Explore the Latest Features of Vue 3',
+        body: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 2,
+        date: 'Jul 15, 2024',
+        title: 'Explore the Latest Features of Vue 3',
+        body: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 2,
+        date: 'Jul 15, 2024',
+        title: 'Explore the Latest Features of Vue 3',
+        body: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    ],
+    total: 10
+  }
 }
 
