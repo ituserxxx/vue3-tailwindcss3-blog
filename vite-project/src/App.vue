681f5e085f24c4e6e391b8432f603d5f08b76f9c
root <root@xiaoxx.utek.com>
2024-09-07 16:18:48 +0800

    debu layout

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue"
 </script>
 <template>
   <HelloWorld />
 </template>
 
-<style scoped>
- 
-</style>
+<style scoped></style>
--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,19 +1,18 @@
 <template>
   <a-layout>
-    <!-- <a-layout-header class="header">
+    <a-layout-header class="header">
       <div class="logo" />
       <a-menu v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
         <a-menu-item key="1">nav 1</a-menu-item>
         <a-menu-item key="2">nav 2</a-menu-item>
         <a-menu-item key="3">nav 3</a-menu-item>
       </a-menu>
-    </a-layout-header> -->
-
-
+    </a-layout-header>
     <a-layout>
       <a-layout-sider width="200" style="background: #fff">
         <a-menu v-model:selectedKeys="selectedKeys2" v-model:openKeys="openKeys" mode="inline"
           :style="{ height: '100%', borderRight: 0 }">
+
           <a-sub-menu key="sub1">
             <template #title>
               <span>
@@ -29,11 +28,11 @@
             <template #title>
               <span>
                 <laptop-outlined />
-                Admin
+                Users
               </span>
             </template>
-            <a-menu-item key="menuAdminAdd">add</a-menu-item>
-            <a-menu-item key="menuAdminList">list</a-menu-item>
+            <a-menu-item key="menuUsersAdd">add</a-menu-item>
+            <a-menu-item key="menuUsersList">list</a-menu-item>
             \
           </a-sub-menu>
 
@@ -45,7 +44,16 @@
               </span>
             </template>
             <a-menu-item key="menuLeaveMessageList">list</a-menu-item>
+          </a-sub-menu>
 
+          <a-sub-menu key="sub4">
+            <template #title>
+              <span>
+                <notification-outlined />
+                Tags
+              </span>
+            </template>
+            <a-menu-item key="menuTagsList">list</a-menu-item>
           </a-sub-menu>
 
         </a-menu>
@@ -57,41 +65,39 @@
           <a-breadcrumb-item>List</a-breadcrumb-item>
           <a-breadcrumb-item>App</a-breadcrumb-item>
         </a-breadcrumb>
-        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-          v-if="selectedKeys2 == menuArticleAdd">
-          <ArticleAddComp />
-        </a-layout-content>
-
-        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-          v-if="selectedKeys2 == menuArticleList">
-          <ArticleListComp />
-        </a-layout-content>
-
-
-        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-          v-if="selectedKeys2 == menuAdminAdd">
-          <AdminAddComp />
-        </a-layout-content>
-        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-          v-if="selectedKeys2 == menuAdminList">
-          <AdminListComp />
+        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+          <component v-if="selectedKeys2.length" :is="compKeyMap[selectedKeys2[0]]" />
         </a-layout-content>
       </a-layout>
+
     </a-layout>
 
   </a-layout>
 </template>
 <script setup>
+import { ref, watch } from 'vue';
+
 import ArticleAddComp from './article/add.vue'
 import ArticleListComp from './article/list.vue'
-import AdminAddComp from './admin/add.vue'
-import AdminListComp from './admin/list.vue'
+import UsersAddComp from './users/add.vue'
+import UsersListComp from './users/list.vue'
+import LeaveMessageListComp from './leaveMessage/list.vue'
+import TagsListComp from './tags/list.vue'
 
-import { ref } from 'vue';
+const compKeyMap = {
+  menuArticleAdd: ArticleAddComp,
+  menuArticleList: ArticleListComp,
+  menuUsersAdd: UsersAddComp,
+  menuUsersList: UsersListComp,
+  menuLeaveMessageList: LeaveMessageListComp,
+  menuTagsList: TagsListComp
+}
+const compSubTitle = {
+  
+}
 
-// const selectedKeys1 = ref([1]);
-const selectedKeys2 = ref(['2']);
-const openKeys = ref(['sub1']);
+const selectedKeys2 = ref([]);
+const openKeys = ref([]);
 </script>
 <style scoped>
 #components-layout-demo-top-side-2 .logo {
