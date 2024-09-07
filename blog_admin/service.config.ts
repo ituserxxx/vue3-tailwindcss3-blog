db40fe83e07eed41280ccdf079e389995fff8b74
root <root@xiaoxx.utek.com>
2024-09-07 13:53:43 +0800

    debu dir

--- blog_admin/service.config.ts
+++ blog_admin/service.config.ts
@@ -1,7 +1,7 @@
 /** 不同请求服务的环境配置 */
 export const serviceConfig: Record<ServiceEnvType, Record<string, string>> = {
   dev: {
-    url: 'https://mock.apifox.cn/m1/4071143-0-default',
+    url: 'http://loclhost:6008/admin',
   },
   test: {
     url: 'https://mock.apifox.cn/m1/4071143-0-default',
