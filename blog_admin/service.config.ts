8645fc9cfa907bb2024e27ff325353d1e75a81a2
root <root@xiaoxx.utek.com>
2024-09-26 09:41:02 +0800

    modity admin

--- blog_admin/service.config.ts
+++ blog_admin/service.config.ts
@@ -1,7 +1,8 @@
 /** 不同请求服务的环境配置 */
 export const serviceConfig: Record<ServiceEnvType, Record<string, string>> = {
   dev: {
-    url: 'http://loclhost:6008/admin',
+    // url: 'http://loclhost:6008/admin',
+    url: 'https://mock.apifox.cn/m1/4071143-0-default',
   },
   test: {
     url: 'https://mock.apifox.cn/m1/4071143-0-default',
