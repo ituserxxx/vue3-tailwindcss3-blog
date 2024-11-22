0cda82bf254643c44c2f7fe0b5b95bdd75b39f70
ituserxxx <66945660+ituserxxx@users.noreply.github.com>
2024-11-22 03:44:49 +0000

    debug filerun

--- deploy/start.env.sh
+++ deploy/start.env.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 DIR="$PWD/mysql"
-# 如果目录不存在，则创建
-[ -d "$DIR" ] || mkdir -p "$DIR"
+[ -d "$DIR" ] || mkdir -p "$DIR"   # 如果目录不存在，则创建
 
 DIR="$PWD/meilisearch"
-# 如果目录不存在，则创建
+[ -d "$DIR" ] || mkdir -p "$DIR" 
+
+DIR="$PWD/filerun/filerun-data"
 [ -d "$DIR" ] || mkdir -p "$DIR"
+
+DIR="$PWD/filerun/filerun-files"
+[ -d "$DIR" ] || mkdir -p "$DIR" 
+
+DIR="$PWD/filerun/db-data"
+[ -d "$DIR" ] || mkdir -p "$DIR" 
 
 docker compose -f $PWD/docker-compose-env.yml up -d
